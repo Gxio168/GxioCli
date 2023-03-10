0e840d5761fe23c58692e817b5af267854315bce
gxio <2632733615@qq.com>
2023-03-10 21:36:56 +0800

    delete dist dir

--- dist/assets/index.5ef40e95.js
+++
@@ -1,1 +0,0 @@
-import"./index.vue_vue_type_script_setup_true_lang.33e0dd64.js";import{_ as t}from"./index.vue_vue_type_script_setup_true_lang.33e0dd64.js";import"./index.021762f1.js";export{t as default};
